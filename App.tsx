d911b8411b58e26f3844cad4c033b07d784bd39f
2salted <noamgraf123@gmail.com>
2024-03-29 02:25:12 -0100

    added setInterval functionality to react native app

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { Button, SafeAreaView, Text } from "react-native";
 
 type catProps = {
   name: string;
 };
 
 const Cat = (props: catProps) => {
+  const [isHungry, setIsHungry] = useState(true);
+
   return (
     <SafeAreaView>
       <Text style={{ fontSize: 18, textAlign: "center" }}>
-        Hello! I am {props.name} and I am{" "}
+        Hello! I am {props.name} and I am {isHungry ? "hungry!" : "full!"}
       </Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+          setInterval(() => {
+            setIsHungry(true);
+          }, 5000);
+        }}
+        title={isHungry ? "Press button to feed the cats" : "The cats are fed"}
+      />
     </SafeAreaView>
   );
 };

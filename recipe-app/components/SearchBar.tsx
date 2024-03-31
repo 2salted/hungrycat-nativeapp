42bf75e779440d0fd32dd8902091557f3efd0e0c
2salted <noamgraf123@gmail.com>
2024-03-31 19:40:37 +0000

    updated search bar and added new component

--- recipe-app/components/SearchBar.tsx
+++ recipe-app/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function SearchBar() {
   return (

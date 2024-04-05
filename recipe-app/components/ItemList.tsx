4f5f2118e2405c373c9d640a5813ecd875368029
2salted <noamgraf123@gmail.com>
2024-04-05 10:41:22 +0000

    added itemlist display when typing added recipe data and added styles to itemlist

--- recipe-app/components/ItemList.tsx
+++ recipe-app/components/ItemList.tsx
@@ -1,16 +1,70 @@
-import { Pressable, Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { recipeData } from "./recipedata";
 
 export default function ItemList() {
   return (
-    <View>
+    <ScrollView>
       {recipeData.map((recipe, index) => {
         return (
-          <Pressable key={index}>
-            <Text>asdads</Text>
-          </Pressable>
+          <TouchableOpacity
+            key={index}
+            style={{
+              flexDirection: "row",
+              width: 230,
+              paddingVertical: 15,
+              paddingLeft: 5,
+            }}
+          >
+            <View style={{}}>
+              <Image
+                style={{
+                  width: 130,
+                  height: 170,
+                  borderRadius: 18,
+                }}
+                source={recipe.image}
+              />
+            </View>
+            <View
+              style={{
+                padding: 15,
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+              }}
+            >
+              <Text
+                numberOfLines={1}
+                style={{
+                  flex: 1,
+                  fontSize: 18,
+                  fontWeight: "600",
+                  alignItems: "flex-start",
+                  justifyContent: "center",
+                }}
+              >
+                {recipe.title}
+              </Text>
+              <Text
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {recipe.titleDescription}
+              </Text>
+            </View>
+          </TouchableOpacity>
         );
       })}
-    </View>
+    </ScrollView>
   );
 }

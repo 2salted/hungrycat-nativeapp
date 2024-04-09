3d5b25571cd07306ffb691b4433c7995f0eca12c
2salted <noamgraf123@gmail.com>
2024-04-09 23:47:11 +0000

    update to dynamic search page for matched selected query

--- recipe-app/app/[id].tsx
+++ recipe-app/app/[id].tsx
@@ -1,6 +1,5 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { Image, SafeAreaView, Text, View, useWindowDimensions } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
-import LeftScreenBtn from "@/components/LeftScreenBtn";
 import RightScreenBtn from "@/components/RightScreentBtn";
 import { recipeData } from "@/components/recipedata";
 
@@ -9,7 +8,8 @@ export default function recipepage() {
 
   return (
     <SafeAreaView style={{
-      flex: 1,
+      width: useWindowDimensions().width,
+      height: useWindowDimensions().height,
       backgroundColor: "white"
     }}>
       <Stack.Screen
@@ -25,9 +25,52 @@ export default function recipepage() {
             return (
               <View key={index}>
                 <View style={{
-                  alignItems: 'center'
+                  paddingTop: 20,
+                  padding: 15,
                 }}>
-                  <Text>{item.title}</Text>
+                  <Text style={{
+                    fontSize: 25,
+                    textAlign: "center",
+                  }}>{item.title}</Text>
+                  <Text style={{
+                    textAlign: "center",
+                    paddingTop: 10
+                  }}>{item.titleDescription}</Text>
+                </View>
+                <View style={{
+                  alignItems: "center"
+                }}>
+                  <Image
+                    resizeMode="cover"
+                    source={item.image}
+                    style={{
+                      width: 300,
+                      height: 170,
+                      borderRadius: 12,
+                      borderWidth: 1,
+                    }}
+                  />
+                </View>
+                <View style={{
+                  paddingTop: 20
+                }}>
+                  <Text style={{
+                    fontSize: 20,
+                    textAlign: "center"
+                  }}>Cooking Info:</Text>
+                  <View style={{
+                    padding: 10,
+                    margin: 20,
+                    flexWrap: 'wrap',
+                    flexDirection: "row",
+                    borderWidth: 1,
+                    borderRadius: 20,
+                  }}>
+                    <Text style={{ padding: 6, textAlign: "center", flexBasis: '50%' }}>prep time: {item.prepTime}</Text>
+                    <Text style={{ padding: 6, textAlign: "center", flexBasis: '50%' }}>cook time: {item.cookingTime}</Text>
+                    <Text style={{ padding: 6, textAlign: "center", flexBasis: '50%' }}>total time: {item.totalTime}</Text>
+                    <Text style={{ padding: 6, textAlign: "center", flexBasis: '50%' }}>servings: {item.servings}</Text>
+                  </View>
                 </View>
               </View>
             )
